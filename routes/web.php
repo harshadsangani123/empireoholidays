3756e52c0e0208ed012abd99946ad0fab8b04419
harshad-nyusoft <harshad.sangani@nyusoft.com>
2026-01-09 19:26:10 +0530

    -Laravel and Intertia Setup.

---
+++ routes/web.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+});
