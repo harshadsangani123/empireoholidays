1872f43ccec5c5299b48df0758e28eff8a63066b
Harshad <sanganiharshad123@yopmail.com>
2026-03-12 22:15:31 +0530

    Implement FilamentUser interface in User model and add access panel method for authorization checks.

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,8 +6,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
@@ -45,4 +47,9 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+    
+    public function canAccessPanel(Panel $panel): bool
+{
+    return true;
+}
 }
